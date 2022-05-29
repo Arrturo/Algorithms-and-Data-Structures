69a91c8413920d202355a1d37cac46f73e3e42b5
Artur <53519645+Arrturo@users.noreply.github.com>
2022-05-29 13:40:12 +0200

    added new methods

--- linked_list.py
+++ linked_list.py
@@ -1,23 +1,27 @@
 from typing import Any
 
 class Node:
-    value: Any
-    next: 'Node'
+    # value: Any
+    # next: 'Node'
 
-    def __init__(self, value, next=None):
+    def __init__(self, value=Any, next=None):
         self.value = value
         self.next = next
 
 class LinkedList:
-    head: Node
-    tail: Node
+    # head: Node
+    # tail: Node
 
     def __init__(self, head=None, tail=None):
         self.head = head
+        self.tail = tail
 
     def push(self, value: Any) -> None:
         node = Node(value, self.head)
         self.head = node
+        if self.tail is None:
+            self.head = node
+            self.tail = node
 
     def append(self, value: Any) -> None:
         node = Node(value)
@@ -28,7 +32,7 @@ class LinkedList:
 
     def node(self, at: int) -> Node:
         counter = 0
-        node = self.head
+        node = self.headp.py
 
         while (counter != at):
             node = node.next
@@ -42,3 +46,25 @@ class LinkedList:
         node.next = after.next
         after.next = node
 
+    def pop(self) -> Any:
+        node = self.head
+        self.head = None 
+        return node
+
+    def remove_last(self) -> Any:
+        node = self.tail
+        self.tail = None
+        return node
+    
+    def remove(self, after: Node) -> Any:
+        after.next = after.next.next
+        after.next.next = None
+        after.next.value = None
+
+    def print(self) -> str:
+        node = self.head
+        head = f'{node.value}'
+        while (node.next != None):
+            node = node.next
+            return head + '->' + f'{node.value}'
+        return head
