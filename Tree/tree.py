e87e3a7d5eca73a0af83024655df58f1a7524db1
Artur <artur.jeznach.8@gmail.com>
2022-07-13 16:29:20 +0200

    added new methods

--- Tree/tree.py
+++ Tree/tree.py
@@ -1,4 +1,5 @@
-from typing import Any, Callable, List
+from typing import Any, Callable, List, Union
+import queue
 
 class TreeNode:
     '''
@@ -26,3 +27,32 @@ class TreeNode:
         if self.children != []:
             for child in self.children:
                 child.for_each_deep_first(visit)
+
+    def for_each_level_order(self, visit: Callable[['TreeNode'], None]) -> None:
+        visit(self)
+        que = queue.Queue()
+        for child in self.children:
+            que.put(child)
+        #que.put(self)
+        while que.empty() == False:
+            element = que.get()
+            visit(element)
+            for child in element.children:
+                que.put(child)
+
+    def search(self, value: Any) -> Union['TreeNode', None]:
+        if value == self.value:
+            return True
+        for child in self.children:
+            if value == child.value:
+                return True
+            child.search(value)
+        return False
+
+class Tree:
+    """
+    root: TreeNode
+    """
+
+    def __init__(self, root: TreeNode):
+        self.root = root
